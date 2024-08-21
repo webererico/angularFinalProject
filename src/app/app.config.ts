d61620ffa9b25edc2ddd3b1ecf84d217adaebbc7
Érico Rosiski Weber <ericoweber@hotmail.com>
2024-08-21 20:23:11 -0300

    aula 9 - login utilizando firebase auth gmail e email

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch()), provideFirebaseApp(() => initializeApp({"projectId":"angularautenticacao","appId":"1:1037586595412:web:e36f3b0d03196ab8b4f7c1","storageBucket":"angularautenticacao.appspot.com","apiKey":"AIzaSyCrCRsteAli_EWMbrJFnHS7XI-wTfcx3qQ","authDomain":"angularautenticacao.firebaseapp.com","messagingSenderId":"1037586595412"})), provideAuth(() => getAuth())]
 };
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth, createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup, signOut, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   private fb = inject(FormBuilder);
+  private auth: Auth = inject(Auth);
+  private provider = new GoogleAuthProvider();
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -35,12 +38,43 @@ export class LoginComponent {
         const jwtToken = response['access_token'];
         console.log(jwtToken);
         localStorage.setItem('JWT', jwtToken);
-        const admin = response['admin'] ??  false;
+        const admin = response['admin'] ?? false;
         console.log(admin);
         localStorage.setItem('admin', admin);
         console.log('passou');
         this.router.navigate(['/home/dashboard']);
       }
+    })
+  }
+
+  loginFirebaseEmail() {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.loginForm.value.email ?? '', this.loginForm.value.password ?? '')
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
+  loginFirebaseGoogle() {
+    signInWithPopup(this.auth, this.provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      console.log(credential);
+      return credential;
+    })
+  }
+
+  logoutFirebase() {
+    signOut(this.auth).then(() => {
+      console.log('signed out');
+    }).catch((error) => {
+      console.log('sign out error: ' + error);
     })
   }
 }
